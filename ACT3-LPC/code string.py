19ddfbcd774b8ff86e7e4990326e7835949d0778
Alex <89739785+zvlnc@users.noreply.github.com>
2021-11-11 08:55:45 -0600

    Se elimino la actividad mal hecha.
    m

--- ACT3-LPC/code string.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-message = "hola_mundo.c"
-message_bytes = message.encode('ascii')
-base64_bytes = base64.b64encode(message_bytes)
-base64_message = base64_bytes.decode('ascii')
-
-print(base64_message)
